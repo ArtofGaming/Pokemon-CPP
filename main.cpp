34d1d417e5f47201d9e63f0f17c655c408cb0da6
ArtofGaming <turner4965@gmail.com>
2024-11-10 17:40:38 -0700

    Added choosing player name

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string playerName;
+    cout << "What's your name?";
+    cin >> playerName;
+    cout << "Good job, " << playerName;
+    return 0;
+}
